e8044c9261c7de29a44951bc372483af45626d82
Sebastian Wehrmann <sebastian.wehrmann@me.com>
2018-02-14 14:00:26 +0100

    Add Changelog

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$HOSTNAME" == "mail" ]]; then
+    sed -i -- "s/\(unreleased\)/`date +%Y-%m-%d`/g" CHANGES.txt
+    rm CHANGES.txt--
+    grunt bump:$@
+    grunt all
+    git add .
+    git ci -am "Release"
+    git checkout production
+    git pull
+    git merge master
+    git checkout master
+    git merge production
+    grunt bump:prerelease
+    sed -i -- '5i\
+X.X.X (unreleased)
+5i\
+==================
+5i\
+\
+- Nothing changed yet.
+5i\
+\
+' CHANGES.txt
+    rm CHANGES.txt--
+    git ci -am "Postrelease"
+fi
