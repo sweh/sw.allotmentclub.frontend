5fcfda41b81fdfe820c3bd4d466704c9b5370ef4
Sebastian Wehrmann <sebastian.wehrmann@me.com>
2019-01-28 09:38:06 +0100

    Release

--- .tmp/concat/optimized.js
+++ .tmp/concat/optimized.js
@@ -105057,7 +105057,7 @@ this["ajja"]["templates"]["upload"] = Handlebars.template({"compiler":[7,">= 4.0
 
 var version = {
     "name": "sw.allotmentclub.frontend",
-    "version": "4.5.1"
+    "version": "4.6.0"
 };
 
 sw.allotmentclub.version = version.version;
--- js/app.baseview.js
+++ js/app.baseview.js
@@ -3,7 +3,7 @@
 
 var version = {
     "name": "sw.allotmentclub.frontend",
-    "version": "4.5.2-dev.0"
+    "version": "4.6.0"
 };
 
 sw.allotmentclub.version = version.version;
